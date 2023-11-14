a175f26d1044bcd6cc547116a12846ad0804f185
harveymorritt <44510513+harveymorritt@users.noreply.github.com>
2023-11-14 02:08:36 +0000

    File I/O, Status Bar and Instrument Settings Added instrument settings (four or two wire, front or back connections). Change inputs to sweep type. Added file I/O settings (cell name and working folder). Measurement will not start without a folder path, instead displaying a warning. Status bar with main states (ready, measuring, measurement finished and ready again).

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import time
+import os
 
 import numpy as np
 
@@ -129,21 +130,27 @@ class DataHandler(QObject):
 
 
 class MainWindow(QMainWindow):
-    startSweepMeasurement = pyqtSignal(np.ndarray)
+    startSweepMeasurementSignal = pyqtSignal(np.ndarray)
 
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Dummy JV")
-
+        # Setting default path for data analysis
+        self.programFolderPath = os.path.abspath(__file__)
+        self.workingFolderPath = ""
+                
         # Sub-widgets
+        self.createInstrumentSettings()
         self.createSweepSettings()
-        self.createMeasurementSettings()
-        self.createMeasurementControls()
+        self.createAnalysisSettings()
+        self.createFileSettings()
+        self.createMeasurementControlButtons()
         self.createDataDisplayTab()
 
         # Main UI
+        self.setWindowTitle("Dummy JV")
         self.buildMainUI()
+        self.statusBar().showMessage("Ready to measure.")
 
         # Mutex objects
         self.mutexMeasurement = QMutex()
@@ -167,55 +174,78 @@ class MainWindow(QMainWindow):
 
         # Measurement Handler to Main GUI Thread
         self.measurementHandler.sendVocValueSignal.connect(self.updateVocValue, Qt.QueuedConnection)
-        self.measurementHandler.measurementSweepStartedSingal.connect(self.disableStartMeasurementButtons, Qt.QueuedConnection)
-        self.measurementHandler.measurementSweepFinishedSignal.connect(self.enableStartMeasurementButtons, Qt.QueuedConnection)
-        self.measurementHandler.measurementVOCStartedSignal.connect(self.disableStartMeasurementButtons, Qt.QueuedConnection)
-        self.measurementHandler.measurementVOCStartedSignal.connect(self.enableStartMeasurementButtons, Qt.QueuedConnection)
+        self.measurementHandler.measurementSweepStartedSingal.connect(self.setDisplayForMeasurementStarted, Qt.QueuedConnection)
+        self.measurementHandler.measurementSweepFinishedSignal.connect(self.setDisplayForMeasurementFinished, Qt.QueuedConnection)
+        self.measurementHandler.measurementVOCStartedSignal.connect(self.setDisplayForMeasurementStarted, Qt.QueuedConnection)
+        self.measurementHandler.measurementVOCStartedSignal.connect(self.setDisplayForMeasurementFinished, Qt.QueuedConnection)
 
         # Data Handler to Main GUI Thread
         self.dataHandler.updateGraphSignal.connect(self.plotData, Qt.QueuedConnection)
 
         # Main GUI Self Connections
-        self.controlStartButton.clicked.connect(self.prepSweepMeasurement, Qt.QueuedConnection)
+        self.controlStartButton.clicked.connect(self.gatekeeperSweepMeasurement, Qt.QueuedConnection)
         self.controlMeasureVoc.clicked.connect(self.measurementHandler.measureVOC, Qt.QueuedConnection)
+        self.inputFetchFolderPath.clicked.connect(self.folderBrowse, Qt.QueuedConnection)
+        self.inputCellArea.valueChanged.connect(self.sendAnalysisVariables, Qt.QueuedConnection)
+        self.inputPower.valueChanged.connect(self.sendAnalysisVariables, Qt.QueuedConnection)
 
         # Main GUI to Measurement Handler
-        self.startSweepMeasurement.connect(self.measurementHandler.measureSweep, Qt.QueuedConnection)
+        self.startSweepMeasurementSignal.connect(self.measurementHandler.measureSweep, Qt.QueuedConnection)
 
         # Console Connections
         self.measurementHandler.sendConsoleUpdateSignal.connect(self.updateConsole, Qt.QueuedConnection)
         self.dataHandler.sendConsoleUpdateSignal.connect(self.updateConsole, Qt.QueuedConnection)
 
+    def createInstrumentSettings(self):
+
+        self.InstrumentSettings = QGroupBox("Instrument Settings")
+        self.layoutInstrumentSettings = QGridLayout(self.InstrumentSettings)
+
+        self.labelFourOrTwoWire = QLabel("Terminal Settings", self.InstrumentSettings)
+        self.inputFourOrTwoWire = QComboBox(self.InstrumentSettings)
+        self.inputFourOrTwoWire.addItem("Four Wire")
+        self.inputFourOrTwoWire.addItem("Two Wire")
+
+        self.labelFrontOrRearPanel = QLabel("Panel Settings", self.InstrumentSettings)
+        self.inputFrontOrRearPanel = QComboBox(self.InstrumentSettings)
+        self.inputFrontOrRearPanel.addItem("Front Banana Connection")
+        self.inputFrontOrRearPanel.addItem("Rear Triaxial Connection")
+
+        self.layoutInstrumentSettings.addWidget(self.labelFourOrTwoWire, 0, 0)
+        self.layoutInstrumentSettings.addWidget(self.inputFourOrTwoWire, 1, 0)
+        self.layoutInstrumentSettings.addWidget(self.labelFrontOrRearPanel, 0, 1)
+        self.layoutInstrumentSettings.addWidget(self.inputFrontOrRearPanel, 1, 1)
+
     def createSweepSettings(self):
 
         self.SweepSettings = QGroupBox("Sweep Settings")
-        self.SweepSettings.setMaximumWidth(150)
+        self.SweepSettings.setMaximumWidth(200)
+        self.SweepSettings.setMaximumHeight(207) # Minimum height of QGroupBox, considering the space needed for it's child widgets
+        self.layoutSweepSettings = QVBoxLayout(self.SweepSettings)
 
-        self.labelStartVoltage = QLabel("Start Voltage (V)")
-        self.inputStartVoltage = QDoubleSpinBox()
+        self.labelStartVoltage = QLabel("Start Voltage (V)", self.SweepSettings)
+        self.inputStartVoltage = QDoubleSpinBox(self.SweepSettings)
         self.inputStartVoltage.setSingleStep(0.01)
         self.inputStartVoltage.setRange(-10, 10)
         self.inputStartVoltage.setValue(1.05)
 
-        self.labelEndVoltage = QLabel("End Voltage (V)")
-        self.inputEndVoltage = QDoubleSpinBox()
+        self.labelEndVoltage = QLabel("End Voltage (V)", self.SweepSettings)
+        self.inputEndVoltage = QDoubleSpinBox(self.SweepSettings)
         self.inputEndVoltage.setSingleStep(0.01)
         self.inputEndVoltage.setRange(-10, 10)
         self.inputEndVoltage.setValue(-0.05)
 
-        self.labelScanRate = QLabel("Scan Rate (mV/s)")
-        self.inputScanRate = QDoubleSpinBox()
+        self.labelScanRate = QLabel("Scan Rate (mV/s)", self.SweepSettings)
+        self.inputScanRate = QDoubleSpinBox(self.SweepSettings)
         self.inputScanRate.setSingleStep(0.01)
         self.inputScanRate.setMinimum(0)
         self.inputScanRate.setValue(2000)
 
-        self.labelSweepType = QLabel("Sweep Type")
-        self.inputSweepType = QComboBox()
-        self.inputSweepType.addItem("Linear")
-        self.inputSweepType.addItem("Reverse")
-        self.inputSweepType.addItem("Symmetric")
+        self.labelSweepType = QLabel("Sweep Type", self.SweepSettings)
+        self.inputSweepType = QComboBox(self.SweepSettings)
+        self.inputSweepType.addItem("Standard")
+        self.inputSweepType.addItem("Hysteresis")
 
-        self.layoutSweepSettings = QVBoxLayout()
         self.layoutSweepSettings.addWidget(self.labelStartVoltage)
         self.layoutSweepSettings.addWidget(self.inputStartVoltage)
         self.layoutSweepSettings.addWidget(self.labelEndVoltage)
@@ -224,66 +254,97 @@ class MainWindow(QMainWindow):
         self.layoutSweepSettings.addWidget(self.inputScanRate)
         self.layoutSweepSettings.addWidget(self.labelSweepType)
         self.layoutSweepSettings.addWidget(self.inputSweepType)
-        self.layoutSweepSettings.addStretch()
-            
-        self.SweepSettings.setLayout(self.layoutSweepSettings)
+        
+    def createAnalysisSettings(self):
 
-    def createMeasurementSettings(self):
+        self.MeasurementSettings = QGroupBox("Analysis Settings")
+        self.MeasurementSettings.setMaximumWidth(self.SweepSettings.maximumWidth())
+        self.MeasurementSettings.setMaximumHeight(self.SweepSettings.maximumHeight())
+        self.layoutMeasurementSettings = QVBoxLayout(self.MeasurementSettings)
 
-        self.MeasurementSettings = QGroupBox("Measurement Settings")
-        self.MeasurementSettings.setMaximumWidth(150)
-
-        self.labelCellArea = QLabel("Cell Area (cm2)")
-        self.inputCellArea = QDoubleSpinBox()
+        self.labelCellArea = QLabel("Cell Area (cm2)", self.MeasurementSettings)
+        self.inputCellArea = QDoubleSpinBox(self.MeasurementSettings)
         self.inputCellArea.setSingleStep(0.01)
         self.inputCellArea.setMinimum(0)
 
-        self.labelPower = QLabel("Light Intensity (units)")
-        self.inputPower = QDoubleSpinBox()
+        self.labelPower = QLabel("Power Input (uWcm-2)", self.MeasurementSettings)
+        self.inputPower = QDoubleSpinBox(self.MeasurementSettings)
         self.inputPower.setSingleStep(0.01)
         self.inputPower.setMinimum(0)
 
-        self.layoutMeasurementSettings = QVBoxLayout()
         self.layoutMeasurementSettings.addWidget(self.labelCellArea)
         self.layoutMeasurementSettings.addWidget(self.inputCellArea)
         self.layoutMeasurementSettings.addWidget(self.labelPower)
         self.layoutMeasurementSettings.addWidget(self.inputPower)
         self.layoutMeasurementSettings.addStretch()
 
-        self.MeasurementSettings.setLayout(self.layoutMeasurementSettings)
+    def createFileSettings(self):
 
-    def createMeasurementControls(self):
+        self.FileSettings = QGroupBox("File I/O Settings")
+        self.layoutFileSettings = QGridLayout(self.FileSettings)
+
+        # Sub-widget for the inputting the name of the cell
+        self.masterInputCellName = QWidget()
+        self.layoutMasterInputCellName = QVBoxLayout(self.masterInputCellName)
+        self.labelCellName = QLabel("Cell Name", self.masterInputCellName)
+        self.inputCellName = QLineEdit(self.masterInputCellName)
+        self.layoutMasterInputCellName.addWidget(self.labelCellName)
+        self.layoutMasterInputCellName.addWidget(self.inputCellName)
+
+        # Sub-widget for button that calls the folder path selector
+        self.masterInputFetchFolderPath = QWidget()
+        self.layoutMasterInputFetchFolderPath = QVBoxLayout(self.masterInputFetchFolderPath)
+        self.labelFetchFolderPath = QLabel("Working Folder Path", self.masterInputFetchFolderPath)
+        self.inputFetchFolderPath = QPushButton("Select...", self.masterInputFetchFolderPath)
+        self.layoutMasterInputFetchFolderPath.addWidget(self.labelFetchFolderPath)
+        self.layoutMasterInputFetchFolderPath.addWidget(self.inputFetchFolderPath)
+
+        # Sub-widget for the folder path display
+        self.masterDisplayFolderPath = QWidget()
+        self.layoutMasterDisplayFolderPath = QVBoxLayout(self.masterDisplayFolderPath)
+        self.labelDisplayFolderPath = QLabel("Working Folder Path", self.masterDisplayFolderPath)
+        self.displayWorkingFolderPath = QPlainTextEdit(self.masterDisplayFolderPath)
+        self.displayWorkingFolderPath.setPlainText(self.workingFolderPath)
+        self.displayWorkingFolderPath.setReadOnly(True)
+        self.displayWorkingFolderPath.setMaximumHeight(300)
+        self.layoutMasterDisplayFolderPath.addWidget(self.labelDisplayFolderPath)
+        self.layoutMasterDisplayFolderPath.addWidget(self.displayWorkingFolderPath, 1) # 1 = strech factor, so disply box streches until maximum height.
+        self.layoutMasterDisplayFolderPath.addStretch()
+        
+        self.layoutFileSettings.addWidget(self.masterInputCellName, 0, 0)
+        self.layoutFileSettings.addWidget(self.masterInputFetchFolderPath, 0, 1)
+        self.layoutFileSettings.addWidget(self.masterDisplayFolderPath, 1, 0, 1, 2)
+
+    def createMeasurementControlButtons(self):
 
         self.MeasurementControls = QGroupBox()
-        
-        self.controlStartButton = QPushButton("Start Measurement")
-        self.controlMeasureVoc = QPushButton("Measure Voc")
-        self.labelVocValue = QLabel("Voc: NaN")
+        self.layoutMeasurementControls = QHBoxLayout(self.MeasurementControls)
 
-        self.layoutMeasurementControls = QHBoxLayout()
+        self.controlStartButton = QPushButton("Start Measurement", self.MeasurementControls)
+        self.controlsStopButton = QPushButton("Stop Measurement", self.MeasurementControls)
+        self.controlMeasureVoc = QPushButton("Measure Voc", self.MeasurementControls)
+        self.labelVocValue = QLabel("Voc: NaN", self.MeasurementControls)
+
         self.layoutMeasurementControls.addWidget(self.controlStartButton)
+        self.layoutMeasurementControls.addWidget(self.controlsStopButton)
         self.layoutMeasurementControls.addWidget(self.controlMeasureVoc)
         self.layoutMeasurementControls.addWidget(self.labelVocValue)
-
-        self.MeasurementControls.setLayout(self.layoutMeasurementControls)
     
     def createDataDisplayTab(self):
  
         # Graph Tab Layout
         self.graphTab = QWidget()
+        layoutGraphTab = QVBoxLayout(self.graphTab)
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
-        graphLayout = QVBoxLayout()
-        graphLayout.addWidget(self.canvas)
-        self.graphTab.setLayout(graphLayout)
+        layoutGraphTab.addWidget(self.canvas)
 
         # Console Tab Layout
         self.consoleTab = QWidget()
+        self.layoutConsoleTab = QVBoxLayout(self.consoleTab)
         self.console = QPlainTextEdit(self.consoleTab)
         self.console.setReadOnly(True)
-        self.consoleLayout = QVBoxLayout()
-        self.consoleLayout.addWidget(self.console)
-        self.consoleTab.setLayout(self.consoleLayout)
+        self.layoutConsoleTab.addWidget(self.console)
 
         # Tab layout (with graph and console)
         self.tabControl = QTabWidget()
@@ -292,26 +353,30 @@ class MainWindow(QMainWindow):
 
     def buildMainUI(self):
 
+        self.mainWidget = QWidget()
+
         # Main layout
-        mainLayout = QGridLayout()
-        mainLayout.addWidget(self.SweepSettings, 0, 0)
-        mainLayout.addWidget(self.MeasurementSettings, 0, 1)
-        mainLayout.addWidget(self.MeasurementControls, 1, 0, 1, 2)
-        mainLayout.addWidget(self.tabControl, 0, 2, 2, 2)
+        self.mainLayout = QGridLayout(self.mainWidget)
+        self.mainLayout.addWidget(self.InstrumentSettings, 0, 0, 1, 2)
+        self.mainLayout.addWidget(self.SweepSettings, 1, 0)
+        self.mainLayout.addWidget(self.MeasurementSettings, 1, 1)
+        self.mainLayout.addWidget(self.FileSettings, 2, 0, 1, 2)
+        self.mainLayout.addWidget(self.MeasurementControls, 3, 0, 1, 2)
+        self.mainLayout.addWidget(self.tabControl, 0, 3, 4, 4)
 
         # Building and showing main widget
-        mainWidget = QWidget()
-        mainWidget.setLayout(mainLayout)
-        self.setCentralWidget(mainWidget)
+        self.setCentralWidget(self.mainWidget)
         self.show()
 
     @pyqtSlot()
-    def disableStartMeasurementButtons(self):
+    def setDisplayForMeasurementStarted(self):
+        self.statusBar().showMessage("Measuring JV sweep...")
         self.controlStartButton.setDisabled(True)
         self.controlMeasureVoc.setDisabled(True)
     
     @pyqtSlot()
-    def enableStartMeasurementButtons(self):
+    def setDisplayForMeasurementFinished(self):
+        self.statusBar().showMessage("Measurement finished, ready to measure.")
         self.controlStartButton.setEnabled(True)
         self.controlMeasureVoc.setEnabled(True)
     
@@ -323,6 +388,7 @@ class MainWindow(QMainWindow):
 
     @pyqtSlot(float)
     def updateVocValue(self, value):
+        print(self.displayWorkingFolderPath.height())
         self.valueVoc = value * 1000
         self.labelVocValue.setText(f"Voc: {self.valueVoc:.0f} mV")
 
@@ -341,23 +407,45 @@ class MainWindow(QMainWindow):
         self.THREAD_Measurement.wait()
         self.THREAD_Data.quit()
         self.THREAD_Data.wait()
+        time.sleep(0.5) # Saftey wait, paranoid step to give everything time to fully shut down.
     
     @pyqtSlot()
-    def prepSweepMeasurement(self):
-        """ Packs the current sweep settings into an array and sends it to the measurement thread"""
+    def sendAnalysisVariables(self):
+        _cellArea = self.inputCellArea.value()
+        _powerIn = self.inputPower.value()
 
-        _startVoltage = self.inputStartVoltage.value() 
-        _endVoltage =  self.inputEndVoltage.value()
-        _scanRate = self.inputScanRate.value()
-        _sweepProperties = np.empty([1, 3])
-        _sweepProperties[0, :] = [_startVoltage, _endVoltage, _scanRate]
+        _analysisVariables = np.empty([1, 2])
+        _analysisVariables = [_cellArea, _powerIn]
 
-        self.startSweepMeasurement.emit(_sweepProperties)
+        #self.sendAnalysisVariablesSignal.emit()
+
+    @pyqtSlot()
+    def gatekeeperSweepMeasurement(self):
+        """Checks if there is a valid path to save data to. Packs the current sweep settings into an array and sends it to the measurement thread."""
+
+        if self.workingFolderPath:
+            _startVoltage = self.inputStartVoltage.value() 
+            _endVoltage =  self.inputEndVoltage.value()
+            _scanRate = self.inputScanRate.value()
+            _sweepProperties = np.empty([1, 3])
+            _sweepProperties[0, :] = [_startVoltage, _endVoltage, _scanRate]
+
+            self.startSweepMeasurementSignal.emit(_sweepProperties)
+        else:
+            self.AlertBox = QMessageBox()
+            self.AlertBox.setWindowTitle("Warning!")
+            self.AlertBox.setText("The working folder path not selected! A working folder path must be set before starting a measurement!")
+            self.AlertBox.exec_()
+            
+    @pyqtSlot()
+    def folderBrowse(self):
+        self.workingFolderPath = QFileDialog.getExistingDirectory(self, "Select Folder")
+        self.displayWorkingFolderPath.setPlainText(self.workingFolderPath)
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
-    app.aboutToQuit.connect(window.shutdown)
+    app.aboutToQuit.connect(window.shutdown, Qt.DirectConnection) # Direct connection to ensure the shutdown method is ran immediately.
     app.exec()
 
 main()
